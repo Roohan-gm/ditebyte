4b96a57c4ca96896ff8660eda668bbb456c5db1a
Roohan <gmroohan@gmail.com>
2025-05-18 10:02:05 +0500

    change api import in userRecipeDetails

--- hooks/userRecipeDetails.js
+++ hooks/userRecipeDetails.js
@@ -1,6 +1,6 @@
 import { useLocalSearchParams } from 'expo-router';
 import { useQuery } from 'convex/react';
-import { api } from '../../convex/_generated/api';
+import { api } from '../convex/_generated/api';
 
 export function useRecipeDetail() {
   const { recipeId } = useLocalSearchParams();
